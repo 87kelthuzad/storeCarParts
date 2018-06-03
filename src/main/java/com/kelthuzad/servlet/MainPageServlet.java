4e28c4a265485bdf7a7b5126842f9732a822ca65
kelthuzad <87kelthuzad@gmail.com>
2018-06-03 15:46:46 +0200

    second commit

--- src/main/java/com/kelthuzad/servlet/MainPageServlet.java
+++ src/main/java/com/kelthuzad/servlet/MainPageServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet( name = "MainPage" , urlPatterns = "/main_page")
+//@WebServlet( name = "MainPage" , urlPatterns = "/main_page")
 public class MainPageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -17,5 +17,7 @@ public class MainPageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletOutputStream out = response.getOutputStream();
         out.println("czesc");
+        out.println("witma");
+        out.println("jestem");
     }
 }
---
+++ src/main/webapp/WEB-INF/mainPage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mateusz
+  Date: 02.06.18
+  Time: 23:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Store with car parts</title>
+</head>
+<body>
+    <h2>Witam</h2>
+</body>
+</html>
